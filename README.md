6b89c69757d3b0868ab2f2d1824a53fe7bce6c93
Mete0 <majitsu@gmail.com>
2016-06-09 13:56:28 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+"# verizonhackathon"  git init git add README.md git commit -m "first commit" git remote add origin https://github.com/Mete0/verizonhackathon.git git push -u origin master
+"# verizonhackathon" 
---
+++ query.js
@@ -0,0 +1,50 @@
+var user_cat;
+$(document).ready(function () {
+    $("#user_input").keyup(function(event){
+        if(event.keyCode == 13){
+            $("#click").click();
+        }
+    });
+
+    $('#search').change(function() {
+        user_cat=$(this).val();
+    });
+    $("button").click(function () {
+        $("#books tbody").empty();
+
+        $.ajax({
+            'url': 'http://localhost:8983/solr/demo/select',
+            'data': {'wt': 'json', 'q': user_cat + ":" + document.getElementById("user_input").value},
+            'success': function (data) {
+                console.log(data.response.docs);
+                console.log(user_cat + ":" + document.getElementById("user_input").value);
+                for (i = 0; i < data.response.docs.length; i++) {
+                    var title = data.response.docs[i].name_txt;
+                    var isbn = data.response.docs[i].id;
+                    var author = data.response.docs[i].author_txt;
+                    var genre = data.response.docs[i].genre_s;
+                    var price = data.response.docs[i].price;
+                    $("#books tbody").append(
+                        '<tr>' +
+                        '<td>'+title+'</td>' +
+                        '<td>'+author+'</td>' +
+                        '<td>'+genre+'</td>' +
+                        '<td>'+price+'</td>' +
+                        '<td>'+isbn+'</td>' +
+                        '</tr> ');
+
+                }
+
+            },
+            'dataType': 'jsonp',
+            'jsonp': 'json.wrf'
+        });
+    });
+});
+
+//function showInput() {
+//
+//    var message_entered =  user_cat + ":" + document.getElementById("user_input").value;
+//
+//    document.getElementById('display').innerHTML = message_entered;
+//}
